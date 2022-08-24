e480a0002f57a3dec6be0e475766c40a0070df0b
rumon5 <mdrumonkhan48519@gmail.com>
2022-08-25 03:27:30 +0600

    Log In UI implementation done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
+import LogIn from "./components/LogIn/LogIn";
 import Navbar from "./components/Navbar/Navbar";
 import SignUp from "./components/SignUp/SignUp";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
         <Route path="/sign-up" element={<SignUp/>}></Route>
+        <Route path="/log-in" element={<LogIn/>}></Route>
         <Route path="*" element={<Error/>}></Route>
       </Routes>
     </div>
--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -2,9 +2,58 @@ import React from 'react';
 
 const LogIn = () => {
     return (
-        <div>
+        <div class="card w-96 bg-white mx-auto p-5 mt-5 shadow-xl">
+      <h3 className="text-2xl text-center">Please Log In!</h3>
+      <div className="mx-auto mt-4">
+        <i
+          title="Continue With Google"
+          class="fa-brands text-5xl mr-3 text-green-600 fa-google cursor-pointer"
+        ></i>
+        <i
+          title="Continue With Github"
+          class="fa-brands text-5xl mr-3 fa-square-github cursor-pointer"
+        ></i>
+        <i
+          title="Continue With Facebook"
+          class="fa-brands text-5xl text-blue-700 fa-facebook cursor-pointer"
+        ></i>
+      </div>
+      <div class="card-body">
+        <form action="">
+          
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+            required
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
             
-        </div>
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+            required
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            
+          </div>
+          
+          <input type="button" className="btn mt-3 w-full" value="Log In" />
+        </form>
+      </div>
+    </div>
     );
 };
 
