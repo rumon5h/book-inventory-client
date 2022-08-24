caf6e82f004a62f8e71fdd662f5cc472cef9f5b7
rumon5 <mdrumonkhan48519@gmail.com>
2022-08-25 03:23:53 +0600

    Sign Up UI implementation done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
         <Route path="*" element={<Error/>}></Route>
       </Routes>
     </div>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React from "react";
 
 const SignUp = () => {
-    return (
-        <div>
+  return (
+    <div class="card w-96 bg-white mx-auto p-5 mt-5 shadow-xl">
+      <h3 className="text-2xl text-center">Create New Account!</h3>
+      <div className="mx-auto mt-4">
+        <i
+          title="Continue With Google"
+          class="fa-brands text-5xl mr-3 text-green-600 fa-google cursor-pointer"
+        ></i>
+        <i
+          title="Continue With Github"
+          class="fa-brands text-5xl mr-3 fa-square-github cursor-pointer"
+        ></i>
+        <i
+          title="Continue With Facebook"
+          class="fa-brands text-5xl text-blue-700 fa-facebook cursor-pointer"
+        ></i>
+      </div>
+      <div class="card-body">
+        <form action="">
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Name</span>
+            </label>
+            <input
+            required
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+           
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+            required
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
             
-        </div>
-    );
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+            required
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Confirm Password</span>
+            </label>
+            <input
+            required
+              type="password"
+              name="confirm-password"
+              id="confirm-password"
+              placeholder="Type here"
+              class="input input-bordered w-full max-w-xs"
+            />
+            
+          </div>
+          <input type="button" className="btn mt-3 w-full" value="Sign Up" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
