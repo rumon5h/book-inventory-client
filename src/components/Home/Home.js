3b93a87733b2ea2a2fe943077a75a0ed8a0c4229
rumon5h <mdrumonkhan48519@gmail.com>
2023-02-07 14:17:16 +0600

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Banner from "../Banner/Banner";
 import Footer from "../Footer/Footer";
 import Services from "../Services/Services";
+import Update from "../Update/Update";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Services />
+      <Update/>
       <Footer />
     </div>
   );
---
+++ src/components/Update/Update.js
@@ -0,0 +1,11 @@
+
+const Update = () => {
+  return (
+    <div className='py-10'>
+        <h2 className='text-4xl pt-5 text-gray-900 font-bold'>Over 200,000 Happy Customers!</h2>
+        <h3 className='text-3xl py-10 text-center font-bold'>Book world has been built from the ground up based directly on user feedback.</h3>
+    </div>
+  )
+}
+
+export default Update;
