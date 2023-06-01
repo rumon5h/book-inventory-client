a66338d96a76d4ec5bfb5d9ba4d8f90f88a17f62
rumon5h <mdrumonkhan48519@gmail.com>
2023-06-02 00:11:20 +0600

    updated

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const Service = () => {
     const {id} = useParams();
     console.log(id);
-    
+    const [book, setBook] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://books-rhrr.onrender.com/api/v1/books/${id}`)
+        .then(res => res.json())
+        .then(data => setBook(data))
+    },[id])
+
+    if(!book){
+        return <Loading/>
+    }
+    console.log(book);
     return (
         <div>
             <h1>This is service page.</h1>
