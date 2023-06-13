104cc51d00098f3e0c4a9ef660ff5b9990704d3a
rumon5h <mdrumonkhan48519@gmail.com>
2023-06-13 07:17:27 -0700

    updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app';
+
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,6 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth
