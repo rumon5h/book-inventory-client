163decbbbcb1e01b17855b2c79a9e118d80d43fa
rumon5h <mdrumonkhan48519@gmail.com>
2023-07-08 09:59:55 -0700

    update count

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -6,6 +6,7 @@ const Service = () => {
     const { id } = useParams();
 
     const [book, setBook] = useState(null);
+    const [count, setCount] = useState(1);
 
     useEffect(() => {
         fetch(`https://books-rhrr.onrender.com/api/v1/books/${id}`)
@@ -16,7 +17,7 @@ const Service = () => {
     if (!book) {
         return <Loading />
     }
-    console.log(book);
+    // console.log(book);
     return (
         book && <article data-aos="zoom-in-up"
             key={book._id}
@@ -24,41 +25,49 @@ const Service = () => {
             className="cursor-pointer shadow-slate-400 mx-auto mt-5 max-w-xl  shadow-lg p-5 rounded-md border-2 "
         >
             <div className='grid grid-cols-2 '>
-            <div>
-                <img
-                    className="rounded-sm h-[270px]"
-                    src={book.image}
-                    alt={book.name}
-                />
-            </div>
-            <div>
-                <h3 className="text-xl font-extrabold text-gray-900">
-                    {book.name}
-                </h3>
-                <p className="text-gray-600 my-3">by {book.author}</p>
-                <p className="text-green-700 my-2 font-extrabold">
-                    {book.available} in stock
-                </p>
-                <p className="text-gray-900 text-3xl my-2 font-extrabold">
-                    $ {book.price}
-                </p>
+                <div>
+                    <img
+                        className="rounded-sm h-[270px]"
+                        src={book.image}
+                        alt={book.name}
+                    />
+                </div>
+                <div>
+                    <h3 className="text-xl font-extrabold text-gray-900">
+                        {book.name}
+                    </h3>
+                    <p className="text-gray-600 my-3">by {book.author}</p>
+                    <p className="text-green-700 my-2 font-extrabold">
+                        {book.available} in stock
+                    </p>
+                    <p className="text-gray-900 text-3xl my-2 font-extrabold">
+                        $ {book.price}
+                    </p>
 
-                <input
-                    defaultValue={1}
-                    className="pl-2 border-2 border-green-500 my-2 rounded-md w-[100px] block outline-none"
-                    type="number"
-                    name="quantity"
-                    id="quantity-input"
-                />
-                <button className="btn btn-sm mb-2 mr-5 inline-block">
-                    <i className="fa-solid fa-circle-minus"></i>
-                </button>
-                <button className="btn btn-sm ">
-                    <i className="fa-solid fa-circle-plus"></i>
-                </button>
-                <button className="btn btn-sm  block">Add To Cart <i className="fa-solid ml-1 fa-cart-arrow-down"></i></button>
-        
-            </div>
+                    <input
+                        value={count}
+                        className="pl-2 border-2 border-green-500 my-2 rounded-md w-[100px] block outline-none"
+                        type="number"
+                        name="quantity"
+                        id="quantity-input"
+                        readOnly
+                    />
+                    {
+                        count >= 1 && <button
+
+                            onClick={() => setCount((prevState) => prevState - 1)}
+
+                            className="btn btn-sm mb-2 mr-5 inline-block">
+                            <i className="fa-solid fa-circle-minus"></i>
+                        </button>}
+                    <button
+                        onClick={() => setCount((prevState) => prevState + 1)}
+                        className="btn btn-sm ">
+                        <i className="fa-solid fa-circle-plus"></i>
+                    </button>
+                    <button className="btn btn-sm  block">Add To Cart <i className="fa-solid ml-1 fa-cart-arrow-down"></i></button>
+
+                </div>
             </div>
             <p className='mt-5'>{book.description}</p>
         </article>
